d0ee2b94ca829c4392c45e34666774d9e5b59de1
Haikalbotz <97148397+Haikalbotz@users.noreply.github.com>
2022-09-23 13:36:29 +0700

    Update konfigurasi.js

--- pengaturan_/konfigurasi.js
+++ pengaturan_/konfigurasi.js
@@ -35,7 +35,7 @@ group: 'Fitur Hanya Bisa Di Gunakan Di Dalam Group !!',
 private: 'Fitur Digunakan Hanya Di Chat Pribadi/Private',
 bot: 'Fitur Hanya Khusus Pengguna Nomor Bot',
 wait: 'L O A D I N G...',
-global.thumb = fs.readFileSync('-+')
+}
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
 fs.unwatchFile(file)
@@ -43,4 +43,5 @@ console.log(chalk.redBright(`perbarui'${__filename}'`))
 delete require.cache[file]
 require(file)
 })
+
 
